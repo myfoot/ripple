421a9650e35408c5644416e787f89ad02e753917
yagince <straitwalk@gmail.com>
2012-12-23 04:24:27 +0900

    ChatRoomを選択可能にした #24

--- test/models/chat/ChatRoomRepositorySpec.scala
+++ test/models/chat/ChatRoomRepositorySpec.scala
@@ -23,6 +23,15 @@ class ChatRoomRepositorySpec extends RepositorySpecBase {
         ChatRoomRepository.find("hogefoobar") must beNone
       }
     }
+    "#find(id)" in {
+      "指定したIDの部屋が存在する場合はSome(ChatRoom)が取得できる" in new withTestData {
+        ChatRoomRepository.find(chatRoom.id) must beSome(chatRoom)
+      }
+      "指定したIDの部屋が存在しない場合はNoneが返る" in new withTestData {
+        ChatRoomRepository.find(-1) must beNone
+      }
+    }
+
     "#findOrCreate" in {
       "指定された名前の部屋が存在する場合はCharRoomオブジェクトを返す" in new withTestData {
         ChatRoomRepository.findOrCreate(chatRoom.name) must equalTo(chatRoom)
@@ -34,19 +43,27 @@ class ChatRoomRepositorySpec extends RepositorySpecBase {
         newRoom.isPersisted must beTrue
       }
     }
+
+    "#all" in {
+      "全ての部屋が取得できる" in new withTestData {
+        ChatRoomRepository.all.size must equalTo(2)
+      }
+    }
   }
 
   trait withTestData extends BeforeAfter {
     lazy val chatRoom = ChatRoom("test_room")
+    lazy val chatRoom2 = ChatRoom("test_room2")
     def before = {
       transaction {
         chatRoom.save
+        chatRoom2.save
       }
     }
 
     def after = {
       transaction {
-        chatRooms.deleteWhere(c => c.id === chatRoom.id)
+        chatRooms.toList.foreach(c => chatRooms.deleteWhere(c2 => c2.id === c.id))
       }
     }
   }
