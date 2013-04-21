bb58e03f8f9dcded76c094e05dcb0cfe81ca6fd9
yagince <straitwalk@gmail.com>
2013-04-21 18:30:07 +0900

    SpecBase refactoring

--- test/models/ModelSpecBase.scala
+++ test/models/ModelSpecBase.scala
@@ -2,8 +2,43 @@ package models
 
 import org.specs2.mutable._
 import org.specs2.mock._
+import org.squeryl.{Session, SessionFactory}
+import org.squeryl.adapters.H2Adapter
+import org.squeryl.PrimitiveTypeMode._
+import models.CoreSchema._
+import org.specs2.execute.{Result, AsResult}
+import play.api.test.Helpers._
+import scala.Some
+import play.api.test.FakeApplication
 
-class ModelSpecBase extends Specification with Mockito {
+class ModelSpecBase extends Specification with Mockito with Around {
+  def before = {}
+  def after = {}
+
+  def around[T](spec: => T)(implicit evidence$1: AsResult[T]): Result = AsResult{
+    running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+      initialDB
+      before
+      val result = spec
+      after
+      result
+    }
+  }
+
+  private def initialDB = {
+     println("Setting up data.")
+     Class.forName("org.h2.Driver")
+     SessionFactory.concreteFactory = Some(() =>
+       Session.create(
+         java.sql.DriverManager.getConnection("jdbc:h2:test/example", "sa", ""),
+         new H2Adapter)
+     )
+     inTransaction {
+       drop
+       create
+       printDdl
+     }
+  }
 
   def verifyRequiredText[A <: BaseEntity](f: String => A) = {
     new RequiredTextTests[A] {
