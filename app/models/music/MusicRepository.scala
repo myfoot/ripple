6823ffff8ed7c23e0a6fa074709cd72ae7a05ae1
yagince <straitwalk@gmail.com>
2013-05-01 21:39:30 +0900

    implemented MusicRepository#find

--- app/models/music/MusicRepository.scala
+++ app/models/music/MusicRepository.scala
@@ -1,12 +1,19 @@
 package models.music
 
 import models.chat.ChatRoom
+import models.CoreSchema._
+import org.squeryl._
+import PrimitiveTypeMode._
 
 object MusicRepository {
+
+  def find(id: Long): Option[Music] = musics.lookup(id)
+
   def insert(chatRoom:ChatRoom, music:Music) = music.validate match {
     case x@Left(_) => x
     case x@Right(_) => {
       Right(chatRoom.musics.associate(music))
     }
   }
+
 }
